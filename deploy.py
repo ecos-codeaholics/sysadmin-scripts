a1c9785e410962a800dfec58ce618a06828ee7f3
Andres Osorio <osorio.af@gmail.com>
2016-10-19 08:16:25 -0500

    [Update] back and front scripts updated

--- deploy.py
+++ deploy.py
@@ -8,36 +8,41 @@ import subprocess
 logging.basicConfig(filename='/var/log/deploy/deploy.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
 logging.info('*')
 
+under_supervision = ['back-factory-dev','front-factory-dev']
+
 def exec_process( cmd ):
 	process  = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 	out, err = process.communicate(cmd)
 	return out, err
 
-def stop_supervisord():
-	command  = 'supervisorctl stop back-factory-test'
+def stop_supervisord(program):
+	command  = 'supervisorctl stop ' + program
 	out, err = exec_process( command )
 	return out, err
 
-def start_supervisord():
-	command = 'supervisorctl start back-factory-test'
+def start_supervisord(program):
+	command = 'supervisorctl start ' + program
 	out, err = exec_process( command )
 	return out, err
 
 backend_script = '/home/ecos/sysadmin-scripts/' + 'deploy_backend.py'
+frontend_script = '/home/ecos/sysadmin-scripts/' + 'deploy_frontend.py'
 
 steps = []
 step0 = 'sudo -u ecos ' + backend_script
+step1 = 'sudo -u ecos ' + frontend_script
+
 steps.append (step0)
+steps.append (step1)
 steps_seq = ';'.join(steps)
 
-#1. stop supervisor 
-stop_supervisord()
+#1. stop under supervision programs
+for pg in under_supervision:
+	stop_supervisord(pg)
 
 #2. execute deployment
 out,err = exec_process( steps_seq )
 
-print out
-print err
-
-#3. start supervisor
-start_supervisord()
+#3. start under supervision programs
+for pg in under_supervision:
+	start_supervisord(pg)
--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#
-#
-#
-#
+# ...
+# Jheison Rodriguez - jheison@uniandes.edu.co
+# ...
 
-target="development"
-frontend_path="/home/ecos/typescript-apps/"+ target +"/factory-of-procedures-front"
+target="development" 
+frontend_path="/home/ecos/typescript-apps/"$target"/factory-of-procedures-front" 
 getcode_cmd="git pull"
 
 cd $frontend_path
 eval $getcode_cmd
-
